1b10c88fc082434ff3f54ca4243cb3c5699565d5
khayym <xeyyam.kerimov@gmail.com>
2022-10-07 18:57:16 +0400

    initial

--- App.js
+++ App.js
@@ -1,10 +1,32 @@
+import 'react-native-gesture-handler';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { useFonts } from 'expo-font';
+import * as SplashScreen from 'expo-splash-screen';
+import { useCallback } from 'react';
+
+SplashScreen.preventAutoHideAsync();
+
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    'Euclid Circular A': require('./assets/font/Euclid-Circular-A-Regular.ttf'),
+  });
+
+  const onLayoutRootView = useCallback(async () => {
+    if (fontsLoaded) {
+      console.log('font installll')
+      await SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded]);
+
+  if (!fontsLoaded) {
+    return null;
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <View style={styles.container} onLayout={onLayoutRootView}>
+      <Text style={{ fontFamily: 'Euclid Circular A' }}>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
   );
